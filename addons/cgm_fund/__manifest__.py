595c134df6dc12a7e2d1cb6537f090bdd5421943
flyingtimes <5200374@qq.com>
2021-07-19 00:56:41 +0800

    remove  unsued

--- addons/cgm_fund/__manifest__.py
+++ addons/cgm_fund/__manifest__.py
@@ -25,11 +25,11 @@
     'data': [
         'security/ir.model.access.csv',
         'views/views.xml',
-        'views/templates.xml',
+        # 'views/templates.xml',
         'views/menu.xml',
     ],
     # only loaded in demonstration mode
     'demo': [
-        'demo/demo.xml',
+        #'demo/demo.xml',
     ],
 }
--- addons/cgm_fund/controllers/controllers.py
+++ addons/cgm_fund/controllers/controllers.py
@@ -1,21 +1,21 @@
 # -*- coding: utf-8 -*-
-from odoo import http
+# from odoo import http
 
 
-class CgmFund(http.Controller):
-    @http.route('/cgm_fund/cgm_fund/', auth='public')
-    def index(self, **kw):
-        return "Hello, world"
+# class CgmFund(http.Controller):
+#     @http.route('/cgm_fund/cgm_fund/', auth='public')
+#     def index(self, **kw):
+#         return "Hello, world"
 
-    @http.route('/cgm_fund/cgm_fund/objects/', auth='public')
-    def list(self, **kw):
-        return http.request.render('cgm_fund.listing', {
-             'root': '/cgm_fund/cgm_fund',
-             'objects': http.request.env['cgm_fund.cgm_fund'].search([]),
-        })
+#     @http.route('/cgm_fund/cgm_fund/objects/', auth='public')
+#     def list(self, **kw):
+#         return http.request.render('cgm_fund.listing', {
+#              'root': '/cgm_fund/cgm_fund',
+#              'objects': http.request.env['cgm_fund.cgm_fund'].search([]),
+#         })
 
-    @http.route('/cgm_fund/cgm_fund/objects/<model("cgm_fund.cgm_fund"):obj>/', auth='public')
-    def object(self, obj, **kw):
-        return http.request.render('cgm_fund.object', {
-             'object': obj
-        })
+#     @http.route('/cgm_fund/cgm_fund/objects/<model("cgm_fund.cgm_fund"):obj>/', auth='public')
+#     def object(self, obj, **kw):
+#         return http.request.render('cgm_fund.object', {
+#              'object': obj
+#         })
